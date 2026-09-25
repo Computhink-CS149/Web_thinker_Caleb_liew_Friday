af82fa1e4e045e5e3bdf9b844e931ea13d74b2ca
Computhink-CS149 <cs149@computhink.com.sg>
2026-09-25 09:51:23 +0000

    Sep 25, 2026, 9:51 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -52,6 +52,7 @@ function draw(){
     text("Enter a adverb",width/2,109);
     text("Enter a place",width/2,134);
     textAlign(CENTER,CENTER);
+    FileList()
     text(storytext,width/2,300);
 }
 
